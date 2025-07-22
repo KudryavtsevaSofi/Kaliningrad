61ba288c6a350b084d95b9202dab781902ee3f7e
Кудрявцева Соня дом <kudryavtseva.sofi@yandex.ru>
2025-07-22 11:49:20 +0400

    реализовано расписание на главной странице без роутинга

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,6 +1,10 @@
 import '../assets/styles/schedule.css';
 import Day1 from '../assets/images/day1.jpg';
 import Day2 from '../assets/images/day2.jpg';
+import Day3 from '../assets/images/day3.jpeg';
+import Day4 from '../assets/images/day4.jpeg';
+import Day5 from '../assets/images/day5.jpg';
+import Day6 from '../assets/images/day6.jpg';
 
 const Schedule = ( ) => {
     return (
@@ -16,7 +20,7 @@ const Schedule = ( ) => {
                             <p className='day-text'>Отдыхаем после перелета и наслаждаемся городскими ландшафтами</p>
                         </div>
                         <img className='day-img-right' src={Day1}></img>
-                        <p className='start'>начать</p>
+                        <p className='start-right'>начать</p>
                     </div>
                     <div className="day2">
                         <div className='text-block-right'>
@@ -24,7 +28,39 @@ const Schedule = ( ) => {
                             <p className='day-text'>Идем в центр</p>
                         </div>
                         <img className='day-img-left' src={Day2}></img>
-                        <p className='start1'>начать</p>
+                        <p className='start-left'>начать</p>
+                    </div>
+                    <div className="day3">
+                        <div className='text-block-left'>
+                            <p className='day-title'>3 день</p>
+                            <p className='day-text'>Погружаемся в историю города-крепости </p>
+                        </div>
+                        <img className='day-img-right' src={Day3}></img>
+                        <p className='start-right'>начать</p>
+                    </div>
+                    <div className="day4">
+                        <div className='text-block-right'>
+                            <p className='day-title'>4 день</p>
+                            <p className='day-text'>Путешествие внутри путешествия: едем в Зеленоградск</p>
+                        </div>
+                        <img className='day-img-left' src={Day4}></img>
+                        <p className='start-left'>начать</p>
+                    </div>
+                    <div className="day5">
+                        <div className='text-block-left'>
+                            <p className='day-title'>5 день</p>
+                            <p className='day-text'>Узнаем культурное наследие - самое время заглянуть в музеи</p>
+                        </div>
+                        <img className='day-img-right' src={Day5}></img>
+                        <p className='start-right'>начать</p>
+                    </div>
+                    <div className="day6">
+                        <div className='text-block-right'>
+                            <p className='day-title'>6 день</p>
+                            <p className='day-text'>Один раз живем - идем в отрыв!</p>
+                        </div>
+                        <img className='day-img-left' src={Day6}></img>
+                        <p className='start-left'>начать</p>
                     </div>
                 </div>
             </div>
