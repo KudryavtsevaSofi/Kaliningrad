9800cd4a8d7122efe75ceaafea8f66ab07897593
Кудрявцева Соня дом <kudryavtseva.sofi@yandex.ru>
2025-07-23 17:31:21 +0400

    сделала динамическую шапку

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,28 @@
 import { useState, useEffect } from 'react';
 import '../assets/styles/header.css';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 
 const Header = ( ) => {
   const [expanded, setExpanded] = useState(false);
+  const location = useLocation();
 
   const handleNavClick = () => {
     setExpanded(false);
+  };
+
+  const getBrandText = () => {
+    if (location.pathname === '/') {
+      return 'Добро пожаловать!';
+    } else
+    return 'Назад';
   };
 
   return (
     <Navbar className='navbar navbar-dark bg-dark' sticky="top" expand="lg" expanded={expanded} onToggle={() => setExpanded(!expanded)}>
       <Container fluid>
         <Navbar.Brand  href="/">
-          <h5>Добро пожаловать!</h5>
+          <h5>{getBrandText()}</h5>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="myNavbar" />
         <Navbar.Collapse id="myNavbar" className="myNavbar">
--- src/pages/day1.js
+++ src/pages/day1.js
@@ -1,7 +1,14 @@
+import Header from '../components/header';
+import Footer from '../components/footer';
+
 const Day1Page = () => {
     return (
         <div className="day1-wrapper">
-            <p>ЭТО 1 ДЕНЬ</p>
+            <Header />
+            <div>
+                
+            </div>
+            <Footer />
         </div>
     );
 };
